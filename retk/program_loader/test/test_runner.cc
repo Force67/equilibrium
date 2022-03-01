e3a42b3f0ddf5f2bdcb5518c0d0c40a85e172f16
Force67 <prelink835@gmail.com>
2022-03-01 22:14:01 +0100

    tweak: kill that folder

--- retk/program_loader/test/test_runner.cc
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2021 Force67 <github.com/Force67>.
-// For licensing information see LICENSE at the root of this distribution.
-
-#include <gtest/gtest.h>
-
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
