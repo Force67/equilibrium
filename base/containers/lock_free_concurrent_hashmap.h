22ded83fd0191f5aa64dfc058b9b932b6d3c3494
Vincent Hengel <prelink835@gmail.com>
2023-11-28 20:42:19 +0100

    Implement iterators for lockinghashmap

--- base/containers/lock_free_concurrent_hashmap.h
+++ base/containers/lock_free_concurrent_hashmap.h
@@ -1,17 +1,73 @@
+// Copyright (C) 2023 Vincent Hengel.
+// For licensing information see LICENSE at the root of this distribution.
 #pragma once
 
+#include <map>
 #include <base/atomic.h>
+#include <base/memory/move.h>
 
 namespace base {
 template <typename Key, typename Value>
 class LockFreeHashMap {
+ public:
+  using value_type = Value;
+  using key_type = Key;
+
+  struct Node;
+
+  class Iterator {
+   private:
+    const LockFreeHashMap<Key, Value>* map;
+    size_t bucketIndex;
+    Node* currentNode;
+
+   public:
+    using KeyValuePair = std::pair<Key, Value>;
+
+    Iterator(const LockFreeHashMap<Key, Value>* map,
+             size_t bucketIndex,
+             Node* node)
+        : map(map), bucketIndex(bucketIndex), currentNode(node) {}
+
+    Iterator& operator++() {
+      if (currentNode) {
+        currentNode = currentNode->next.load();
+      }
+      while (!currentNode && bucketIndex < map->bucketCount - 1) {
+        ++bucketIndex;
+        currentNode = map->buckets[bucketIndex].load();
+      }
+      return *this;
+    }
+
+    KeyValuePair& operator*() { return currentNode->keyValue; }
+    KeyValuePair* operator->() { return &currentNode->keyValue; }
+
+    // Equality and inequality operators
+    bool operator==(const Iterator& other) const {
+      return currentNode == other.currentNode;
+    }
+    bool operator!=(const Iterator& other) const { return !(*this == other); }
+  };
+
+  Iterator begin() {
+    for (size_t i = 0; i < bucketCount; ++i) {
+      Node* node = buckets[i].load();
+      if (node) {
+        return Iterator(this, i, node);
+      }
+    }
+    return end();
+  }
+
+  Iterator end() { return Iterator(this, bucketCount, nullptr); }
+
  private:
   struct Node {
-    Key key;
-    Value value;
+    std::pair<Key, Value> keyValue;
     base::Atomic<Node*> next;
 
-    Node(Key k, Value v) : key(k), value(v), next(nullptr) {}
+    Node(Key k, Value &&v) : keyValue(std::make_pair(k, base::move(v))), next(nullptr) {}
   };
 
   base::Atomic<Node*>* buckets;
@@ -20,9 +76,6 @@ class LockFreeHashMap {
   size_t hash(Key key) const { return key % bucketCount; }
 
  public:
-  using value_type = Value;
-  using key_type = Key;
-
   LockFreeHashMap(size_t count) : bucketCount(count) {
     buckets = new std::atomic<Node*>[count];
     for (size_t i = 0; i < count; ++i) {
@@ -32,7 +85,7 @@ class LockFreeHashMap {
 
   void insert(Key key, Value value) {
     size_t index = hash(key);
-    Node* newNode = new Node(key, value);
+    Node* newNode = new Node(key, base::move(value));
     Node* oldHead = buckets[index].load();
 
     do {
@@ -45,8 +98,8 @@ class LockFreeHashMap {
     Node* head = buckets[index].load();
 
     while (head) {
-      if (head->key == key) {
-        value = head->value;
+      if (head->keyValue.first == key) {
+        value = head->keyValue.second;
         return true;
       }
       head = head->next;
@@ -61,7 +114,7 @@ class LockFreeHashMap {
     Node* prev = nullptr;
 
     while (head) {
-      if (head->key == key) {
+      if (head->keyValue.first == key) {
         Node* next = head->next;
         if (prev) {
           prev->next = next;
