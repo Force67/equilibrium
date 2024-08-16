a53bcf9ffcb5f33c6e4996f4286ef09add289c61
Vincent Hengel <prelink835@gmail.com>
2024-08-16 16:18:30 +0200

    y

--- build/premake_linux.sh
+++ build/premake_linux.sh
@@ -16,13 +16,13 @@ PREMAKE_BIN="$DIR/bin/linux/premake5"
 PREMAKE_FILE="$DIR/../premake5.lua"
 
 if [[ $# -eq 0 ]]; then
-    echo 'No target configuration provided (e.g., debug, release).'
+    echo 'No target configuration provided (e.g., debug, release, etc.).'
     exit 1
 fi
 
 BUILD_MODE=$1
 
-if [[ "$BUILD_MODE" != "debug" && "$BUILD_MODE" != "release" ]]; then
+if [[ "$BUILD_MODE" != "debug" && "$BUILD_MODE" != "release" && "$BUILD_MODE" != "shipping" ]]; then
     echo "Invalid build mode: $BUILD_MODE. Allowed values are: debug, release."
     exit 1
 fi
