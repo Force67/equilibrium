c60be22ee491d5900ed6e6dcd063adbb9608267e
Vincent Hengel <prelink835@gmail.com>
2026-03-21 11:20:52 +0100

    fix: include export.h in memory_coordinator.h

--- base/allocator/memory_coordinator.h
+++ base/allocator/memory_coordinator.h
@@ -7,6 +7,7 @@
 #ifdef BASE_MAY_USE_MEMORY_COORDINATOR
 
 #include <base/compiler.h>            //< for STRONG_INLINE
+#include <base/export.h>
 #include <base/profiling/profiler.h>  //< for BASE_PROFILE_ALLOCATION
 
 #include <base/allocator/allocator_primitives.h>
